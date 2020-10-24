030416ff57b52a29b2e8c0c50fe05d800f13ee4a
Andres Bravo <andres_f5@hotmail.com>
2020-10-24 13:58:35 -0500

    prueba ReactJS con algunas modificaciones para el correcto funcionamiento

--- src/components/TableWithInputs.js
+++ src/components/TableWithInputs.js
@@ -5,71 +5,32 @@ import { Button, Col, Form, Row } from 'react-bootstrap'
 export default function TableWithInputs(props) {
 
     const { data, setData } = props
-    const [value, setValue] = useState(null)
 
-
-    const updateData = (e) => {
-
-        setValue(e.target.value)
-        if (value === null) {
-            setValue(e.target.value)
-            let init = e.target.value
-            console.log(init)
-
-        }
-        let idCategoria = e.target.id
-        let idVariable = e.target.name
-
-        if (idCategoria === '') {
-            idCategoria = 0
-        }
-        idCategoria = parseInt(idCategoria, 10)
-        idVariable = parseInt(idVariable, 10)
-
-        let categoria = data[idCategoria].name
+    const updateData = (e, idx, id) => {
         let myData = [...data]
-
-        let newArray = []
-        newArray = myData[idCategoria].variables
-
-        newArray[idVariable] = value
-
-        console.log(newArray)
-        myData[idCategoria] = {
-            name: categoria,
-            variables: newArray,
-        }
-        //console.log(myData)
+        myData[idx].variables[id] = e.target.value
         setData(myData)
-
     }
-
 
     return (
         <div>
             <Form>
                 <Form.Group>
                     <Form.Label><h3> Tabla de variables modificable </h3></Form.Label>
-
                     <Row>
                         {props.data.map((info, idx) => (
                             <Col key={idx} >
                                 <Row>   {info.name} </Row>
                                 { info.variables ? info.variables.map((variable, id) =>
                                     <Row key={id}>
-                                        <Form.Control defaultValue={variable} id={idx} name={id} onChange={updateData} type="text" />
+                                        <Form.Control value={data[idx].variables[id]} id={idx} name={id} onChange={(e) => updateData(e, idx, id)} type="text" />
                                     </Row>
                                 ) : null}
                             </Col>
                         ))}
                     </Row>
-
-
-
                 </Form.Group>
-
             </Form>
-
         </div>
 
 
